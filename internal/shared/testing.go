69c1c3885a00ee72e96627a07a5fef0b8f42e89d
Massimo Costa <costa.massimo@gmail.com>
2024-11-15 14:14:10 +0100

    test: make NATS server startup faster

--- internal/shared/testing.go
+++ internal/shared/testing.go
@@ -23,17 +23,7 @@ func NewNATSServerAndConn(t *testing.T) (*server.Server, *nats.Conn) {
 
 	go s.Start()
 
-	for i := 0; i < 5 && !s.Running(); i++ {
-		t.Logf("Running %v", s.Running())
-		time.Sleep(time.Second)
-	}
-	if !s.Running() {
-		s.Shutdown()
-		s.WaitForShutdown()
-		t.Fatal("not yet running")
-	}
-
-	if !s.ReadyForConnections(10 * time.Second) {
+	if !s.ReadyForConnections(5 * time.Second) {
 		log.Fatal("NATS server not ready in time")
 	}
 
